828a775d580221a0ae2ad7a1dbe2efda7031dc7b
lonefelidae16 <kow161206@gmail.com>
2023-03-27 11:57:16 +0900

    [enhancement] more chat input sound
    * update `chat.ChatHudMixin`
      - add scroll sound
    
    * update `typing.SelectionManagerMixin`
      - paste action sound takes priority
    
    * new mixin
      - `typing.SuggestionWindowMixin`
      - `typing.ChatScreenMixin`

--- src/main/java/dev/stashy/extrasounds/mixin/chat/ChatHudMixin.java
+++ src/main/java/dev/stashy/extrasounds/mixin/chat/ChatHudMixin.java
@@ -10,25 +10,44 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.message.MessageSignatureData;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatHud.class)
 public abstract class ChatHudMixin {
+    @Shadow
+    private int scrolledLines;
+    @Shadow
+    private @Final MinecraftClient client;
+
+    @Unique
+    private int currentLines;
+
     @Inject(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At("RETURN"))
-    private void messageSound(Text message, @Nullable MessageSignatureData signature, int ticks, @Nullable MessageIndicator indicator, boolean refresh, CallbackInfo ci) {
-        if (MinecraftClient.getInstance().player == null || refresh) {
+    private void extrasounds$receiveMessage(Text message, @Nullable MessageSignatureData signature, int ticks, @Nullable MessageIndicator indicator, boolean refresh, CallbackInfo ci) {
+        final ClientPlayerEntity player = this.client.player;
+        if (player == null || refresh) {
             return;
         }
 
         String msg = message.getString();
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
         if (msg.contains("@" + player.getName().getString()) || msg.contains("@" + player.getDisplayName().getString())) {
             SoundManager.playSound(Sounds.CHAT_MENTION, SoundType.CHAT_MENTION);
         } else {
             SoundManager.playSound(Sounds.CHAT, SoundType.CHAT);
+        }
+    }
+
+    @Inject(method = "scroll", at = @At("RETURN"))
+    private void extrasounds$onScroll(int amount, CallbackInfo ci) {
+        if (this.scrolledLines != this.currentLines) {
+            SoundManager.playSound(Sounds.INVENTORY_SCROLL, SoundType.CHAT);
+            this.currentLines = this.scrolledLines;
         }
     }
 }
---
+++ src/main/java/dev/stashy/extrasounds/mixin/typing/SuggestionWindowMixin.java
@@ -0,0 +1,44 @@
+package dev.stashy.extrasounds.mixin.typing;
+
+import dev.stashy.extrasounds.SoundManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatInputSuggestor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ChatInputSuggestor.SuggestionWindow.class)
+public abstract class SuggestionWindowMixin {
+    @Shadow
+    private int selection;
+    @Shadow
+    private boolean completed;
+
+    @Unique
+    private int currentPos;
+
+    @Inject(method = "select", at = @At("RETURN"))
+    private void extrasounds$suggestionSelect(int index, CallbackInfo ci) {
+        if (this.selection != this.currentPos) {
+            SoundManager.keyboard(SoundManager.KeyType.CURSOR);
+            this.currentPos = this.selection;
+        }
+    }
+
+    @Inject(method = "complete", at = @At("HEAD"))
+    private void extrasounds$suggestionComplete(CallbackInfo ci) {
+        if (this.completed) {
+            return;
+        }
+        SoundManager.keyboard(SoundManager.KeyType.INSERT);
+    }
+
+    @Inject(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ChatInputSuggestor;clearWindow()V"))
+    private void extrasounds$closeWindow(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        SoundManager.keyboard(SoundManager.KeyType.CURSOR);
+    }
+}
