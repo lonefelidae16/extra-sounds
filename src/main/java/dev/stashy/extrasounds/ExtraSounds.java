d722d08a46ba1cd0b1f8d28be89ab5ce3b2155b9
lonefelidae16 <kow161206@gmail.com>
2023-04-06 10:17:11 +0900

    [refactor] creating SoundEvents
    * add helper method in `ExtraSounds`
      - `createEvent`: the wrapper method of `SoundEvent#of`

--- src/main/java/dev/stashy/extrasounds/ExtraSounds.java
+++ src/main/java/dev/stashy/extrasounds/ExtraSounds.java
@@ -1,15 +1,20 @@
 package dev.stashy.extrasounds;
 
+import com.mojang.logging.LogUtils;
 import dev.stashy.extrasounds.debug.DebugUtils;
 import dev.stashy.extrasounds.mapping.SoundPackLoader;
 import dev.stashy.extrasounds.sounds.SoundType;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 
 public class ExtraSounds implements ClientModInitializer {
+    public static final Logger LOGGER = LogUtils.getLogger();
     public static final String MODID = "extrasounds";
+    public static final SoundEvent MISSING = SoundEvent.of(new Identifier(MODID, "missing"));
     static final Random mcRandom = Random.create();
 
     @Override
@@ -24,5 +29,14 @@ public class ExtraSounds implements ClientModInitializer {
             return null;
         }
         return new Identifier(MODID, "%s.%s.%s".formatted(type.prefix, id.getNamespace(), id.getPath()));
+    }
+
+    public static SoundEvent createEvent(String path) {
+        try {
+            return SoundEvent.of(new Identifier(MODID, path));
+        } catch (Throwable ex) {
+            LOGGER.error("[%s] Failed to create SoundEvent".formatted(ExtraSounds.class.getSimpleName()), ex);
+        }
+        return MISSING;
     }
 }
