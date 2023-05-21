7bccd2c8b211221c00eb64ea06913e71b17956ec
lonefelidae16 <kow161206@gmail.com>
2023-05-21 15:17:25 +0900

    [update] fit to new API
    * update language files
      - all keys have been changed

--- src/main/java/dev/stashy/extrasounds/Mixers.java
+++ src/main/java/dev/stashy/extrasounds/Mixers.java
@@ -3,13 +3,12 @@ package dev.stashy.extrasounds;
 import dev.stashy.soundcategories.CategoryLoader;
 import net.minecraft.sound.SoundCategory;
 
-public class Mixers implements CategoryLoader
-{
-    @Register(id = "EXTRASOUNDS_MASTER", master = true, defaultLevel = 0.5f)
+public class Mixers implements CategoryLoader {
+    @Register(master = true, defaultLevel = 0.5f)
     public static SoundCategory MASTER;
     @Register
     public static SoundCategory INVENTORY;
-    @Register(tooltip = "tooltip.soundCategory.action")
+    @Register(tooltip = "tooltip.soundCategory.extrasounds_action")
     public static SoundCategory ACTION;
     @Register
     public static SoundCategory CHAT;
